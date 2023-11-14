18cc387bca4908ab5e46c96bc82c7663f104ab0d
Mastermind <148354429+GibsonHolben@users.noreply.github.com>
2023-11-14 08:19:33 -0500

    Initial commit

---
+++ Address.java
@@ -0,0 +1,136 @@
+/**
+ * A class used to hold the address of a building
+ * @author Gibson Holben
+ * @version 0.0.3
+ */
+public class Address
+{	
+	/**The City that the building is located in
+	 * Default = Blank*/
+	private String City = " ";
+	
+	/**The state that the building is located in
+	 * Default = Blank*/
+	private String State = "";
+	
+	/** The street and house number of the building
+	 * Default = Blank*/
+	private String StreetAddress = "";
+	
+	/**The Zip Code of the building
+	 * Default = 0*/
+	private int zipCode = 0;
+	
+	/**
+	 * Overides the default constructor
+	 */
+	Address()
+	{
+		
+		
+	}
+	
+	/**
+	 * An overloaded constructor the creates an 
+	 * Address with all the variables passed in
+	 * @param StreetAddress The new StreetAddress
+	 * @param City The new City
+	 * @param zipCode The new zipCode
+	 */
+	Address(String StreetAddress, String State, String City, int zipCode)
+	{
+		this.StreetAddress  = 			StreetAddress;
+		this.State 			= 			State;
+		this.City 			= 			City;
+		this.zipCode 		= 			zipCode;
+	}
+
+	/**
+	 * Returns the strees address of the BUilding
+	 * @return StreetAddress
+	 */
+	public String getStreetAddress() {
+		return StreetAddress;
+	}
+
+	/**
+	 * Sets the strees address of the Building
+	 * @param streetAddress the new Address of the buildings
+	 */
+	public void setStreetAddress(String streetAddress) {
+		StreetAddress = streetAddress;
+	}
+
+	/**
+	 * Returns the City that the building is located in
+	 * @return City
+	 */
+	public String getCity() {
+		return City;
+	}
+
+	/**
+	 * Sets the city that the Building is located in
+	 * @param city the new City
+	 */
+	public void setCity(String city) {
+		City = city;
+	}
+
+	/**
+	 * Returns the state that the building is located in
+	 * @return State
+	 */
+	public String getState() {
+		return State;
+	}
+
+	/**
+	 * Sets the State that the building is located in
+	 * @param state the new State
+	 */
+	public void setState(String state) {
+		State = state;
+	}
+
+	/**
+	 * Returns the zip code of the building
+	 * @return zipCode
+	 */
+	public int getZipCode() {
+		return zipCode;
+	}
+
+	/**
+	 * Sets the zip code of the Building
+	 * @param zipCode the new zip code
+	 */
+	public void setZipCode(int zipCode) {
+		this.zipCode = zipCode;
+	}
+	
+	
+	/**
+	 * Returns the variables of Address in a CSV formated String
+	 * @return StreetAddress|City|zipCode
+	 */
+	public String toString()
+	{
+		//Merges the Strings
+		String s = StreetAddress + "|" + State + "|" + City + "|" + zipCode;
+		
+		//Returns them
+		return s;
+	}
+	
+	/**
+	 * Prints the variables to the console in an orderly fashion 
+	 */
+	public void printAll()
+	{
+		System.out.println("StreetAdress: " +  StreetAddress);
+		System.out.println("State: " + State);
+		System.out.println("City: " + City);
+		System.out.println("zipCode: " + zipCode);
+	}
+}
---
+++ AddressDriver.java
@@ -0,0 +1,64 @@
+/**
+ * Tests the Address class
+ * @author Gibson Holben
+ * @version 0.0.2
+ */
+public class AddressDriver
+{
+
+	/**
+	 * The main method of the driver
+	 * @param args Unused
+	 */
+	public static void main(String[] args) 
+	{
+		//Testing the dafualt constructor
+		System.out.println("**********************************A1");
+		Address A1 = new Address();
+		System.out.println("A1");
+		System.out.println("************************************\n");
+		
+		//Testing the overloaded constructor
+		System.out.println("**********************************A2");
+		Address A2 = new Address("4934 Pear Lane", "Mi", "Clarkston", 48353);
+		testGettersAndSetters(A2);
+		System.out.println("************************************\n");
+		
+		//Test toString
+		System.out.println("**********************************A3");
+		Address A3 = new Address("8534 rock Lane", "FL", "Orlando", 47293);
+		System.out.println(A3.toString());
+		System.out.println("************************************\n");
+		
+		//Test toString
+		System.out.println("**********************************A4");
+		Address A4 = new Address("842397 boring road", "MI", "Madison Heights", 58202);
+		A4.printAll();
+		System.out.println("************************************\n");
+		
+	}
+	
+	/**
+	 * Tests the getters and setters on a Address
+	 * @param a the passed in Address
+	 */
+	public static void testGettersAndSetters(Address a)
+	{
+		System.out.println(a.getCity());
+		a.setCity("Detroit");
+		System.out.println(a.getCity());
+		
+		System.out.println(a.getStreetAddress());
+		a.setStreetAddress("210 Bannana Road");
+		System.out.println(a.getStreetAddress());
+		
+		System.out.println(a.getState());
+		a.setState("WA");
+		System.out.println(a.getState());
+		
+		System.out.println(a.getZipCode());
+		a.setZipCode(43085);
+		System.out.println(a.getZipCode());
+	}
+
+}
