47e3f496c24fd199d6e26bfd71a1ef1afff7e58b
Mastermind <148354429+GibsonHolben@users.noreply.github.com>
2023-11-16 09:44:31 -0500

    Updated files
    Changed and added code to the filles

--- School.java
+++ School.java
@@ -3,10 +3,12 @@
  * based off of the Building class
  * <b>Extends: Building</b>
  * @author <p>Gibson Holben</p>
- * @version 0.0.0.1
+ * @version 0.0.0.5
  */
 public class School extends Building 
 {
+	
+	//Instance vars
 	/**The schools name
 	 * Default = blank*/
 	private String 	Name = "";
@@ -15,7 +17,7 @@ public class School extends Building
 	 * Default = 1*/
 	private short 	numOfClassrooms = 1;
 	
-	/**THe number of stuents in each class
+	/**The number of stuents in each class
 	 * Default = 1*/
 	private short 	numOfStudentsPerClass = 1;
 	
@@ -27,7 +29,7 @@ public class School extends Building
 	 * Default = Blank*/
 	private String 	ParkingLotType = "";
 	
-	
+	//Constructors
 	/**
 	 * The default constructor
 	 */
@@ -37,7 +39,17 @@ public class School extends Building
 	}
 	
 	
-	
+	/**
+	 * Overloaded constructor that creates a School with
+	 *  a address and the parking lot
+	 *  
+	 * @param a1 The address object
+	 * @param Floors The number of floors that the building has
+	 * @param Name The name of the school
+	 * @param numOfParkingLotSpaces The number of
+	 * parking spaces in the parking lot
+	 * @param ParkingLotType THe type of parking lot 
+	 */
 	School(Address a1,
 			int Floors, 
 			String Name, 
@@ -51,7 +63,28 @@ public class School extends Building
 	}
 	
 	
-	
+	/**
+	 * Overloaded constructor that creates a school and takes 
+	 * in all the address variables, the building variables,
+	 * and the school variables
+	 * 
+	 * @param StreetAddress The new StreetAddress
+	 * @param City The new City
+	 * @param State the state that the building is in
+	 * @param zip The new zipCode
+	 * @param Floors How many floors that are in the building.
+	 * @param Foundation The type of foundation for the building
+	 * @param Roof The type of roof on the building.
+	 * @param Rooms How many rooms that are in the building.
+	 * @param length The length of the building.
+	 * @param width The width of the building.
+	 * @param Name The name of the school
+	 * @param numOfParkingLotSpaces The number of
+	 * parking spaces in the parking lot
+	 * @param ParkingLotType THe type of parking lot 
+	 * @param numOfStudentsPerClass The number of students per classroom
+	 * @param numOfClassrooms The number of classrooms in the school
+	 */
 	School(String StreetAddress, 
 			String State,
 			String City, 
@@ -78,4 +111,125 @@ public class School extends Building
 		this.numOfClassrooms = numOfClassrooms;
 		this.numOfStudentsPerClass = numOfStudentsPerClass;
 	}
+
+	
+	//Getters and setters
+
+	/**
+	 * 
+	 * @return the name
+	 */
+	public String getName() {
+		return Name;
+	}
+
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		Name = name;
+	}
+
+
+	/**
+	 * @return the numOfClassrooms
+	 */
+	public short getNumOfClassrooms() {
+		return numOfClassrooms;
+	}
+
+
+	/**
+	 * @param numOfClassrooms the numOfClassrooms to set
+	 */
+	public void setNumOfClassrooms(short numOfClassrooms) {
+		this.numOfClassrooms = numOfClassrooms;
+	}
+
+
+	/**
+	 * @return the numOfStudentsPerClass
+	 */
+	public short getNumOfStudentsPerClass() {
+		return numOfStudentsPerClass;
+	}
+
+
+	/**
+	 * @param numOfStudentsPerClass the numOfStudentsPerClass to set
+	 */
+	public void setNumOfStudentsPerClass(short numOfStudentsPerClass) {
+		this.numOfStudentsPerClass = numOfStudentsPerClass;
+	}
+
+
+	/**
+	 * @return the numOfParkingLotSpaces
+	 */
+	public short getNumOfParkingLotSpaces() {
+		return numOfParkingLotSpaces;
+	}
+
+
+	/**
+	 * @param numOfParkingLotSpaces the numOfParkingLotSpaces to set
+	 */
+	public void setNumOfParkingLotSpaces(short numOfParkingLotSpaces) {
+		this.numOfParkingLotSpaces = numOfParkingLotSpaces;
+	}
+
+
+	/**
+	 * @return the parkingLotType
+	 */
+	public String getParkingLotType() {
+		return ParkingLotType;
+	}
+
+
+	/**
+	 * @param parkingLotType the parkingLotType to set
+	 */
+	public void setParkingLotType(String parkingLotType) {
+		ParkingLotType = parkingLotType;
+	}
+
+
+	/**
+	 * Returns the variables of all the superclass
+	 *  and the superclasses hasA is a CSV format
+	 * @return super.getA1().toString() + super.toString() 
+	 * + schools variables
+	 */
+	public String toString() 
+	{
+		String s1 = Name + "|" + numOfClassrooms +
+				"|" + numOfStudentsPerClass + "|"
+				+ numOfParkingLotSpaces + "|" + ParkingLotType; 
+		return super.getA1().toString() + super.toString() + s1 ;
+	}
+
+	/**
+	 * Prints all the variables from the superclass
+	 *  and the superclasses hasA in an ordrly fassion
+	 */
+	public void printAll()
+	{
+		super.getA1().printAll();
+		super.printAll();
+		System.out.println("numOfParkingLotSpaces: " + numOfParkingLotSpaces);
+		System.out.println("ParkingLotType: " + ParkingLotType);
+		System.out.println("numOfClassrooms: " + numOfClassrooms);
+		System.out.println("numOfStudentsPerClass: " + numOfStudentsPerClass);
+	}
+	
+	
+	
+	public long calcTotalStudents()
+	{
+		long l = numOfStudentsPerClass * numOfClassrooms;
+		return l;
+	}
+	
 }
--- SchoolDriver.java
+++ SchoolDriver.java
@@ -1,7 +1,7 @@
 /**
  * A test case for the School class
  * @author Gibson Holben
- * @version 0.0.0.1
+ * @version 0.0.0.3
  *
  */
 public class SchoolDriver
@@ -15,12 +15,14 @@ public class SchoolDriver
 		System.out.println("**************************************\n");
 		
 		System.out.println("**************************************S2");
+		//Testing the middle constructor
 		Address A1 = new Address("4934 Pear2 Lane", "Clarkston2", "MI", 48353);
 		School s2 = new School(A1, 4, "OSTC", (short) 130, "Concreete");
 		System.out.println("**************************************\n");
 		
 		
 		System.out.println("**************************************S3");
+		//Testing the main constructor
 		School s3 = new School("532 gibson road", 
 								"MI",
 								"Gibsonville",
@@ -37,8 +39,26 @@ public class SchoolDriver
 								(short) 14,
 								(short) 18);
 		
+		
+		System.out.println(s3.toString());
+		
+		System.out.println("Testing printAll");
+		
+		s3.printAll();
+		
+		System.out.println("Testing the calcTotalStudents");
+		System.out.println("Number of classrooms: " + s3.getNumOfClassrooms());
+		System.out.println("number of stuents per classroom: " + s3.getNumOfStudentsPerClass());
+		
+		
 		System.out.println("**************************************\n");
 
+	}
+	
+	
+	public static void testGettersNadSetters(School s)
+	{
+		
 	}
 
 }
